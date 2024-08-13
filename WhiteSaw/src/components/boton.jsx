5f82444731b1bbaf7af9f7bd701f9d05f80dad84
sierralevi <fransierra230705@gmail.com>
2024-08-12 21:16:11 -0300

    probando componentes

---
+++ WhiteSaw/src/components/boton.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { isMuiElement } from "@mui/material";
+import { withEmotionCache } from "@emotion/react";
+import  from "@emotion/styled";
+
+<ButtonGroup variant="contained" aria-label="Basic button group">
+  <Button>One</Button>
+  <Button>Two</Button>
+  <Button>Three</Button>
+</ButtonGroup>
